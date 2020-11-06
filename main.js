4642a16e5290680ce109fec03be2b1c4667fefc1
Roma Levsha <rm.levsha@gmail.com>
2020-11-06 14:26:33 +0300

    Feat: add choice size line thickness, choice color

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const canvas = document.querySelector('#myCanvas'); //get canvas from doc
+const inputs = document.querySelectorAll(".control input");
+const turnOn = document.querySelector('#turnOn');
+console.log(turnOn.value);
 const ctx = canvas.getContext('2d'); //create a drawing object
+let color = '#BADA55';
+let size = 1;
 canvas.width = window.innerWidth; //get all width not includes scrollWidth
-canvas.height = window.innerHeight; // get all width not includes scrollWidth
-ctx.strokeStyle = '#BADA55'; //set color for strokes when does not set with hsl calculator
+canvas.height = window.innerHeight - 30; // get all width not includes scrollWidth
+ctx.strokeStyle = color; //set color for strokes when does not set with hsl calculator
 ctx.lineJoin = 'round'; //Creates a rounded corner when the two lines meet
 ctx.lineCap = 'round'; // Draw a line with rounded end caps
-ctx.lineWidth = 100; //The lineWidth property sets  the current line width, in pixels.
+ctx.lineWidth = size; //The lineWidth property sets  the current line width, in pixels.
 ctx.globalCompositeOperation = 'source-over';
 
 let isDrawing = false; //default means
@@ -14,10 +19,17 @@ let lastY = 0; //default means
 let hue = 0; //default means (one of the main properties (called color appearance parameters) of a color 0-360)
 let direction = true; //demind for check condition for iteration;
 
+function handleUpdate(e) {
+  ctx.strokeStyle = this.value;
+  ctx.lineWidth = this.value;
+}
+
 function draw(e) {
   if (!isDrawing) return; // stop the fn from running when they are not moused down
   console.log(e);
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; //HSL Color Calculator
+  if (turnOn.value === '0') {
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  } //HSL Color Calculator
   ctx.beginPath();
   // start from
   ctx.moveTo(lastX, lastY);
@@ -30,7 +42,8 @@ function draw(e) {
   if (hue >= 360) {
     hue = 0;
   }
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+  if (turnOn.value === '0'){
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
     direction = !direction;
   }
 
@@ -39,6 +52,7 @@ function draw(e) {
   } else {
     ctx.lineWidth--;
   }
+}
 
 }
 
@@ -47,7 +61,11 @@ canvas.addEventListener('mousedown', (e) => {
   [lastX, lastY] = [e.offsetX, e.offsetY]; //set x and y in the moment down (remind 0,0);
 });
 
-
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
+
+
