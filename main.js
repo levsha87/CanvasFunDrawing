25d805797d5a5a53853b095b5be8c5c3d1086ca2
Roma Levsha <rm.levsha@gmail.com>
2020-11-17 10:57:56 +0300

    Fix: fix mistakes: getDefaultValues, const COLOR, SIZE, single quotes

--- main.js
+++ main.js
@@ -1,91 +1,90 @@
 function initFunCanvasApp() {
-const canvas = document.querySelector('#myCanvas'); //get canvas from doc
-const inputs = document.querySelectorAll(".control input");
-const manualModeOn = document.querySelector('#manualModeOn');
+  const canvas = document.querySelector('#myCanvas'); 
+  const inputs = document.querySelectorAll('.control input');
+  const manualModeCheckBox = document.querySelector('#manualModeCheckBox');
+  const ctx = canvas.getContext('2d'); 
+  const COLOR = document.querySelector('#lineColor').value;
+  const SIZE = document.querySelector('#lineSize').value;
 
+  canvas.width = window.innerWidth; 
+  canvas.height = window.innerHeight - 40; 
+  ctx.strokeStyle = COLOR; 
+  ctx.lineJoin = 'round'; 
+  ctx.lineCap = 'round'; 
+  ctx.lineWidth = SIZE; 
+  ctx.globalCompositeOperation = 'source-over';
 
-const ctx = canvas.getContext('2d'); //create a drawing object
-let color = '#BADA55';
-let size = 2;
-canvas.width = window.innerWidth; //get all width not includes scrollWidth
-canvas.height = window.innerHeight - 30; // get all width not includes scrollWidth
-ctx.strokeStyle = color; //set color for strokes when does not set with hsl calculator
-ctx.lineJoin = 'round'; //Creates a rounded corner when the two lines meet
-ctx.lineCap = 'round'; // Draw a line with rounded end caps
-ctx.lineWidth = size; //The lineWidth property sets  the current line width, in pixels.
-ctx.globalCompositeOperation = 'source-over';
+  let isDrawing = false; 
+  let lastX = 0; 
+  let lastY = 0; 
+  let hue = 0; 
+  let direction = true; 
 
-let isDrawing = false; //default means
-let lastX = 0; //default means
-let lastY = 0; //default means
-let hue = 0; //default means (one of the main properties (called color appearance parameters) of a color 0-360)
-let direction = true; //demind for check condition for iteration;
-
-function handleUpdate(e) {
-  ctx.strokeStyle = this.value;
-  ctx.lineWidth = this.value;
-}
-
-function defaultValue (){
-  ctx.strokeStyle = color;
-  ctx.lineWidth = size;
-}
-
-function draw (e){
-  if(manualModeOn.checked){
-    drawManualMode(e);
-  } else {
-    drawRainbow(e);
-  } 
-}
-
-function drawRainbow(e) {
-  if (!isDrawing) return; // stop the fn from running when they are not moused down
-  
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  //HSL Color Calculator
-  ctx.beginPath();
-  // start from
-  ctx.moveTo(lastX, lastY);
-  // go to
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY]; //define current x, y
-
-  hue++;
-  if (hue >= 360) {
-    hue = 0;
+  function handleUpdate(e) {
+    ctx.strokeStyle = this.value;
+    ctx.lineWidth = this.value;
   }
-  
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction;
+
+  function getDefaultValue() {
+    ctx.strokeStyle = COLOR;
+    ctx.lineWidth = SIZE;
   }
-    if(direction) {
-    ctx.lineWidth++;
+
+  function draw(e) {
+    if (manualModeCheckBox.checked) {
+      drawManualMode(e);
     } else {
-    ctx.lineWidth--;
+      drawRainbow(e);
+    }
   }
-}
 
-function drawManualMode(e) {
-  if (!isDrawing) return;
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-}
+  function drawRainbow(e) {
+    if (!isDrawing) return; 
 
-canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true; 
-  [lastX, lastY] = [e.offsetX, e.offsetY]; //set x and y in the moment down (remind 0,0);
-});
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    //HSL Color Calculator
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY]; //define current x, y
 
-inputs.forEach((input) => input.addEventListener("change", handleUpdate));
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
-manualModeOn.addEventListener('click', defaultValue);
+    hue++;
+    if (hue >= 360) {
+      hue = 0;
+    }
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+      direction = !direction;
+    }
+    if (direction) {
+      ctx.lineWidth++;
+    } else {
+      ctx.lineWidth--;
+    }
+  }
+
+  function drawManualMode(e) {
+    if (!isDrawing) return;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  }
+
+  canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY]; 
+  });
+
+  inputs.forEach((input) => input.addEventListener('change', handleUpdate));
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', () => (isDrawing = false));
+  canvas.addEventListener('mouseout', () => (isDrawing = false));
+  manualModeCheckBox.addEventListener('click', getDefaultValue);
 }
 
 initFunCanvasApp();
